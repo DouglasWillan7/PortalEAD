b1f1d031c620ffccf86ee0e4f6f413af0ffd3e08
Douglas Willan <douglaswillan7@gmail.com>
2017-10-18 00:33:31 -0200

    send img for courses

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -1,8 +1,14 @@
-from django.conf.urls import url, include
+from django.conf.urls import url, include, patterns
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
-    url(r'^', include('simplemooc.core.urls', namespace='core')),
-    url(r'^courses/', include('simplemooc.courses.urls', namespace='courses')),
-    url(r'^admin/', admin.site.urls),
-]
+urlpatterns = patterns('',
+                       url(r'^', include('simplemooc.core.urls', namespace='core')),
+                       url(r'^cursos/', include('simplemooc.courses.urls', namespace='courses')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
