c1d59b03078cb89fce347a57b7b9f5740dd0063d
Douglas Willan <douglaswillan7@gmail.com>
2017-10-18 23:02:58 -0200

    adding new field in courses. adding page course details

--- simplemooc/courses/urls.py
+++ simplemooc/courses/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, patterns, include
 
 urlpatterns = patterns('simplemooc.courses.views',
                        url(r'^$', 'index', name='index'),
+                       url(r'^(?P<pk>\d+)/$', 'details', name='details'),
 
                        )
 
