1ffbfaeb4d10378cbbd5a16f9f382e9958677549
Douglas Willan <douglaswillan7@gmail.com>
2017-10-20 00:06:15 -0200

    forms and send e-mail

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 
 def index(request):
     courses = Course.objects.all()
@@ -21,8 +22,17 @@ def index(request):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
-    context = {
-        'course': course
-    }
+    context = {}
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            form.send_mail(course)
+            form = ContactCourse()
+    else:
+        form = ContactCourse()
+
+    context['form'] = form
+    context['course'] = course
     template_name = 'courses/details.html'
     return render(request, template_name, context)
